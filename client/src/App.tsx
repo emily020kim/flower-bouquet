bfe70d4815943b40efaf561f5e6947771cdeb30b
emily020kim <emily020kim@gmail.com>
2024-05-06 10:49:45 -0700

    api and client setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,16 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
-import './App.css'
+import Home from "./pages/Home";
 
 function App() {
 
-
   return (
     <div>
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -38,10 +38,28 @@ app.get("/orchid", (req, res) => {
     )
 })
 
-app.get("/orchid", (req, res) => {
+app.get("/dahlia", (req, res) => {
     res.json(
-        { name: "Orchid", genus: "Orchidaceae", season: "Winter, Spring", water: "Water every 1-2 weeks", sunlight: "Indirect sunlight" }
+        { nname: "Dahlia", genus: "Dahlia", season: "Spring, Autumn", water: "Water once a week", sunlight: "Direct sunlight" }
     )
 })
+app.get("/hyacinth", (req, res) => {
+    res.json(
+        { name: "Hyacinth", genus: "Hyacinthus", season: "Spring", water: "Water every 1-2 weeks", sunlight: "Full sun" }
+    )
+})
+
+app.get("/lily", (req, res) => {
+    res.json(
+        { name: "Lily", genus: "Lilium", season: "Summer, Autumn", water: "Water every 2-3 days", sunlight: "Direct sunlight" }
+    )
+})
+
+app.get("/carnation", (req, res) => {
+    res.json(
+        { name: "Carnation", genus: "Caryophyllaceae", season: "Spring, Autumn", water: "Water once a week", sunlight: "Full sun" }
+    )
+})
+
 
 app.listen(5000, () => { console.log("Server started on port 5000") })
