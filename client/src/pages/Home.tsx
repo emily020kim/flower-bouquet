e6081006d5f352d6d9b78f624a0ecc6ff5466788
emily020kim <emily020kim@gmail.com>
2024-05-07 08:59:04 -0700

    finished home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,19 +2,27 @@ import { useState, Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Flowers from "../models/Flowers";
 import Loader from '../components/Loader';
+import { PiFlowerThin } from "react-icons/pi";
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
 
   const adjustFlowersForScreenSize = () => {
     let screenScale = null;
-    const screenPosition = [-2.65, -2.5, -0.5];
+    let screenPosition = [0, 0, 0];
     const rotation = [0.2, 0, 0];
 
     if (window.innerWidth < 768) {
-      screenScale = [0.3, 0.3, 0.3];
-    } else {
       screenScale = [4, 4, 4];
+      screenPosition = [-0.2, -4, -0.5];
+    } 
+    if (window.innerWidth === 768) {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -2, -0.5];
+    } 
+    if (window.innerWidth > 768) {
+      screenScale = [3.5, 3.5, 3.5];
+      screenPosition = [0, -2, -0.5];
     }
 
     return [screenScale, screenPosition, rotation];
@@ -23,27 +31,39 @@ const Home = () => {
   const [flowersScale, flowersPosition, flowersRotation] = adjustFlowersForScreenSize();
 
   return (
-    <section className='w-full h-screen relative'>
-      <Canvas 
-        className={`w-full h-screen ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
-        camera={{ near: 0.1, far: 1000 }}
-      >
-        <Suspense fallback={<Loader />}>
-          <directionalLight position={[1, 1, 1]} intensity={2} />
-          <ambientLight intensity={0.5} />
-          <hemisphereLight groundColor="#000000" intensity={1} />
+    <div className='flex flex-col h-screen md:flex-row w-full'>
+      <section className='w-full md:w-1/2 h-full md:h-screen relative'>
+        <Canvas 
+          className={`w-full h-screen ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
+          camera={{ near: 0.1, far: 1000 }}
+        >
+          <Suspense fallback={<Loader />}>
+            <directionalLight position={[1, 1, 1]} intensity={2} />
+            <ambientLight intensity={0.5} />
+            <hemisphereLight groundColor="#000000" intensity={1} />
 
-          <Flowers
-            position={flowersPosition}
-            scale={flowersScale}
-            rotation={flowersRotation}
-            isRotating={isRotating}
-            setIsRotating={setIsRotating}
-          />
-        </Suspense>
-      </Canvas>
+            <Flowers
+              position={flowersPosition}
+              scale={flowersScale}
+              rotation={flowersRotation}
+              isRotating={isRotating}
+              setIsRotating={setIsRotating}
+            />
+          </Suspense>
+        </Canvas>
+      </section>
 
-    </section>
+      <div className='flex flex-col w-full md:w-1/2 h-full md:h-screen items-center justify-center'>
+        <h1 className='font-serif font-semibold text-2xl md:text-3xl lg:text-4xl text-red-300'>Emily's Bouquet</h1>
+        <button
+          className='flex bg-red-400 items-center px-2 py-3 rounded-md text-white font-normal font-serif mt-3'
+          //add route
+        >
+          Welcome <PiFlowerThin size={20} className="text-white ml-1" />
+        </button>
+      </div>
+    </div>
+    
   )
 }
 
